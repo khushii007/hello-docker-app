0693811583ef2a419166b43883fd9bcee20797de
khushii007 <khushi.israni07@gmail.com>
2025-06-05 14:26:51 +0000

    Switch to Flask

--- app/main.py
+++ app/main.py
@@ -1,20 +1,10 @@
-from http.server import BaseHTTPRequestHandler, HTTPServer
+from flask import Flask
 
-HOST, PORT = "0.0.0.0", 8080
+app = Flask(__name__)
 
-
-class HelloHandler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        # Always return 200 and a friendly message
-        self.send_response(200)
-        self.send_header('Content-Type', 'text/plain; charset=utf-8')
-        self.end_headers()
-        # Encode the Unicode string to UTF-8 bytes
-        message = "👋 Hello from inside a Docker container!"
-        self.wfile.write(message.encode('utf-8'))
-
+@app.route('/')
+def hello():
+    return "👋 Hello from inside a Docker container!"
 
 if __name__ == "__main__":
-    httpd = HTTPServer((HOST, PORT), HelloHandler)
-    print(f"Serving on http://{HOST}:{PORT}")
-    httpd.serve_forever()
+    app.run(host="0.0.0.0", port=8080)
